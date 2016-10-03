5d7777de5a85a7b512c6667cee7e1ec1d37fc8de
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2016-10-03 16:15:58 +1000

    Added profile script; replace expensive filter() with trim()

--- ts/app.ts
+++ ts/app.ts
@@ -6,12 +6,15 @@ const fs = require('fs')
 const os = require('os')
 import * as R from 'ramda'
 const WorkerPool = require('node-worker-pool')
+const log = console.log
+const time = console.time
+const timeEnd = console.timeEnd
 
 const inputStream = fs.createReadStream(process.argv[2], { encoding: 'utf8' })
 const outputStream = fs.createWriteStream(process.argv[3])
 
 const numberOfWorkers = os.cpus().length
-console.log(`numberOfWorkers: ${numberOfWorkers}`)
+log(`numberOfWorkers: ${numberOfWorkers}`)
 const workerPool = new WorkerPool(
   numberOfWorkers,
   process.execPath, // path to the node binary
@@ -22,12 +25,12 @@ const workerPool = new WorkerPool(
 let chunkCount = 0
 let chunksProcessing = 0
 
-console.time('inputStream')
+time('inputStream')
 inputStream.on('data', async (chunk: string) => {
   chunkCount++
   chunksProcessing++
 
-  let cards: string[] = R.filter((x) => x !== '', R.split('\n', chunk))
+  let cards: string[] = R.split('\n', chunk.trim())
   let message: Message = { data: cards }
   let reply: Message = await workerPool.sendMessage(message)
 
@@ -37,14 +40,14 @@ inputStream.on('data', async (chunk: string) => {
   if (chunksProcessing === 0) {
     // The last chunk has been processed - exit
     outputStream.end()
-    console.time('workerPoolDestroy')
+    time('workerPoolDestroy')
     await workerPool.destroy()
-    console.timeEnd('workerPoolDestroy')
-    console.timeEnd('program')
+    timeEnd('workerPoolDestroy')
+    timeEnd('program')
   }
 })
 
 inputStream.on('end', () => {
-  console.timeEnd('inputStream')
-  console.log(`chunkCount: ${chunkCount}`)
+  timeEnd('inputStream')
+  log(`chunkCount: ${chunkCount}`)
 })
