47da34fccdca36b954e57c7a53cf59d021660376
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2016-10-09 22:20:10 +1000

    Added tests for credit_cards lib

--- ts/lib/credit_cards.spec.ts
+++ ts/lib/credit_cards.spec.ts
@@ -1,11 +1,42 @@
 /// <reference path="../../node_modules/@types/mocha/index.d.ts"/>
 /// <reference path="../../node_modules/@types/chai/index.d.ts"/>
+
 import { expect } from 'chai'
+import { validateCards } from './credit_cards'
 
 describe('credit_cards', function() {
-  describe('VISA', function() {
-    it('truth', function() {
-      expect(1 + 1).to.eql(2)
-    })
+  it('AMEX', function() {
+    const cards = [ '378282246310005' ]
+    const cardDescriptions = [ 'AMEX: 378282246310005 (invalid)' ]
+
+    expect(validateCards(cards)).to.eql(cardDescriptions)
+  })
+
+  it('Discover', function() {
+    const cards = [ '6011111111111117' ]
+    const cardDescriptions = [ 'Discover: 6011111111111117 (valid)' ]
+
+    expect(validateCards(cards)).to.eql(cardDescriptions)
+  })
+
+  it('MasterCard', function() {
+    const cards = [
+      '5105105105105100',
+      '5105105105105106'
+    ]
+
+    const cardDescriptions = [
+     'MasterCard: 5105105105105100 (valid)',
+     'MasterCard: 5105105105105106 (invalid)'
+    ]
+
+    expect(validateCards(cards)).to.eql(cardDescriptions)
+  })
+
+  it('Unknown', function() {
+    const cards = [ '9111111111111111' ]
+    const cardDescriptions = [ 'Unknown: 9111111111111111 (invalid)' ]
+
+    expect(validateCards(cards)).to.eql(cardDescriptions)
   })
 })
--- ts/lib/credit_cards.ts
+++ ts/lib/credit_cards.ts
@@ -1,15 +1,5 @@
 import * as R from 'ramda'
 
-// Test cases:
-//    VISA: 4111111111111111 (valid)
-//    VISA: 4111111111111 (invalid)
-//    VISA: 4012888888881881 (valid)
-//    AMEX: 378282246310005 (invalid)
-//    Discover: 6011111111111117 (valid)
-//    MasterCard: 5105105105105100 (valid)
-//    MasterCard: 5105105105105106 (invalid)
-//    Unknown: 9111111111111111 (invalid)
-
 export function validateCards(cards: string[]): string[] {
   return R.map(validateCard, cards)
 }
