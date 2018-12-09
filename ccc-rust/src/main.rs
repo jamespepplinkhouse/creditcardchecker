4b4b2f518ee8d8060ad2ea915056d19edafb1303
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2018-12-09 19:40:18 +1000

    Working without card brand ident (slow)

--- ccc-rust/src/main.rs
+++ ccc-rust/src/main.rs
@@ -1,3 +1,29 @@
+use luhn2::validate;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_file_arg = &args[1];
+    let output_file_arg = &args[2];
+
+    let input_file = File::open(input_file_arg).expect("Cannot open the input file!");
+    let cards = BufReader::new(input_file);
+
+    let results: Vec<String> = cards
+        .lines()
+        .filter_map(|result| result.ok())
+        .map(|card| {
+            let validity = match validate(card.parse::<u64>().unwrap()) {
+                false => "invalid",
+                true => "valid",
+            };
+
+            format!("MasterCard: {} ({})", card, validity)
+        })
+        .collect();
+
+    let mut output = File::create(output_file_arg).expect("Cannot create output file!");
+    write!(output, "{}\n", results.join("\n"));
 }
